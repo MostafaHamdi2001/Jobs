17596e6020f468632a5c45dced8bfab62b0bd3f2
Mostafa Hamdi <mostafahamdi689@gmail.com>
2025-12-16 16:44:31 +0200

    Update Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        // تشغيل الأمر المخصص يومياً في تمام الساعة 01:00
+        $schedule->command('products:inactivate-old')->dailyAt('01:00');
     }
 
     /**
@@ -20,7 +21,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
