ad7f2caf12812d8186360e7c2862999eacc4c950
Mostafa Hamdi <mostafahamdi689@gmail.com>
2025-12-21 16:47:07 +0200

    Added ProcessProductCreated

---
+++ app/Jobs/ProcessProductCreated.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Jobs;
+use App\Models\Product;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class ProcessProductCreated implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    public function handle()
+    {
+        Log::info("Processing product: " . $this->product->name);
+    }
+}
