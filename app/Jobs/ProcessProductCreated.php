dd46b2ce926bf6595da565af75c89923a33f1385
Mostafa Hamdi <mostafahamdi689@gmail.com>
2025-12-23 16:16:16 +0200

    Update ProcessProductCreated

--- app/Jobs/ProcessProductCreated.php
+++ app/Jobs/ProcessProductCreated.php
@@ -1,15 +1,17 @@
 <?php
-
 namespace App\Jobs;
+
 use App\Models\Product;
+use App\Models\Color;
+use App\Mail\ProductCreatedMail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
-class ProcessProductCreated implements ShouldQueue
+class ProcessProductCreation implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
@@ -22,6 +24,10 @@ class ProcessProductCreated implements ShouldQueue
 
     public function handle()
     {
-        Log::info("Processing product: " . $this->product->name);
+        $colorIds = Color::pluck('id')->toArray();
+        $this->product->colors()->sync($colorIds);
+
+        Mail::to(config('mail.from.address'))
+            ->send(new ProductCreatedMail($this->product));
     }
 }
