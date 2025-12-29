a136baf50713744b124b8505ec0921c88c9628a4
Mostafa Hamdi <mostafahamdi689@gmail.com>
2025-12-29 18:35:33 +0200

    Update Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'title_en',
@@ -25,5 +27,8 @@ class Product extends Model
         return $this->belongsToMany(Color::class);
     }
 
-    
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
 }
