bbc32aada991f6104638c659312f42d40e35795a
Mostafa Hamdi <mostafahamdi689@gmail.com>
2025-12-29 18:36:08 +0200

    Update Products table

--- database/migrations/2025_12_11_140637_create_products_table.php
+++ database/migrations/2025_12_11_140637_create_products_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string("title_en");
             $table->string("title_ar");
             $table->boolean("active")->default(true);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
