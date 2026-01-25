20977ac48ee55da284ec12d32a186a143250352c
Mostafa Hamdi <mostafahamdi689@gmail.com>
2026-01-25 20:26:31 +0200

    Edit in PreventRequestsDuringMaintenance.php

--- app/Http/Middleware/PreventRequestsDuringMaintenance.php
+++ app/Http/Middleware/PreventRequestsDuringMaintenance.php
@@ -2,16 +2,37 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance as Middleware;
 
 class PreventRequestsDuringMaintenance extends Middleware
 {
     /**
-     * The URIs that should be reachable while maintenance mode is enabled.
-     *
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'login',
+        'api/login',
+        'logout',
+        'api/logout',
     ];
+
+   
+    public function handle($request, Closure $next)
+    {
+        if ($request->isMethod('get')) {
+            return parent::handle($request, $next);
+        }
+
+        foreach ($this->except as $except) {
+            if ($request->fullUrlIs($except) || $request->is($except)) {
+                return parent::handle($request, $next);
+            }
+        }
+
+        return response()->json([
+            'status' => 'error',
+            'message' => 'عذراً، الموقع حالياً في وضع القراءة فقط. لا يمكن الإضافة أو التعديل.'
+        ], 403);
+    }
 }
